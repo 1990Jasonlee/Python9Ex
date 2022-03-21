4b9b6f2b26c070a4a68de7187a2b8e1a24ed37a4
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-20 20:16:33 -0400

    part b done

--- data/json_helper.py
+++ data/json_helper.py
@@ -5,4 +5,12 @@ def read_json(path):
     json_object = json.load(open_json)
     return json_object
 
-def read_all_json_files():
+def read_all_json_files(path):
+    json_list = []
+    for files in os.walk(path):
+        for file_in_question in files:
+            if file_in_question.endswith('.json'):
+                result = read_json(os.path.join(path, file_in_question))
+                json_list.append(result)
+    return json_list
+
